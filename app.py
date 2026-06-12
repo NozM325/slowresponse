f3ac457ca8bc6d6670f683155cb86f785ba66855
Nozipho Masango <72254200+NozM325@users.noreply.github.com>
2026-06-12 11:32:02 +0200

    replace file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/test-slow-endpoint")
 def test_slow_endpoint():
-    #time.sleep(310)  # 310 seconds = 5+ minutes
+    time.sleep(310)  # 310 seconds = 5+ minutes
     return "done"
 
 if __name__ == "__main__":
