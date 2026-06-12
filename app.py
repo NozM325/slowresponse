8de646c40ce60be8f314f4d869780dbfb139b525
Nozipho Masango <72254200+NozM325@users.noreply.github.com>
2026-06-12 11:14:32 +0200

    Added files

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route("/test-slow-endpoint")
+def test_slow_endpoint():
+    #time.sleep(310)  # 310 seconds = 5+ minutes
+    return "done"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
