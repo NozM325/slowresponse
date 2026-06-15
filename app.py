380211880340b8b5d78cab4706f2fcffd667fa73
Nozipho Masango <72254200+NozM325@users.noreply.github.com>
2026-06-15 14:33:34 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+import random
 from flask import Flask
 import time
-import random
 
 app = Flask(__name__)
 
@@ -9,3 +9,6 @@ def test_slow_endpoint():
     if random.random() < 0.3:  # 30% of requests
         time.sleep(70)         # > 60 seconds
     return "done"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
